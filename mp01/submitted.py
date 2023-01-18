fe6d3f83cfffef09c341460506838867eb0c4137
williamcheng <csh1234_1@126.com>
2023-01-18 17:42:02 -0600

    complete mp01

--- mp01/submitted.py
+++ mp01/submitted.py
@@ -8,6 +8,7 @@ function's docstring.
 
 import numpy as np
 
+
 def joint_distribution_of_word_counts(texts, word0, word1):
     '''
     Parameters:
@@ -20,8 +21,19 @@ def joint_distribution_of_word_counts(texts, word0, word1):
       X0 is the number of times that word1 occurs in a given text,
       X1 is the number of times that word2 occurs in the same text.
     '''
-    raise RuntimeError('You need to write this part!')
+    d = dict()  # key: word count tuple, value: number of texts
+    for text in texts:
+        word0_cnt = text.count(word0)
+        word1_cnt = text.count(word1)
+        d[(word0_cnt, word1_cnt)] = d.get((word0_cnt, word1_cnt), 0) + 1
+
+    m = max(d.keys(), key=lambda a: a[0])[0]
+    n = max(d.keys(), key=lambda a: a[1])[1]
+    Pjoint = np.array([[0.0 for _ in range(n + 1)] for _ in range(m + 1)])
+    for k0, k1 in d:
+        Pjoint[k0][k1] = d[(k0, k1)] / len(texts)
     return Pjoint
+
 
 def marginal_distribution_of_word_counts(Pjoint, index):
     '''
@@ -36,9 +48,19 @@ def marginal_distribution_of_word_counts(Pjoint, index):
       if index==0, then X is X0
       if index==1, then X is X1
     '''
-    raise RuntimeError('You need to write this part!')
+    l = len(Pjoint) if index == 0 else len(Pjoint[0])
+    Pmarginal = np.array([0.0 for _ in range(l)])
+    for x in range(l):
+        if index == 0:
+            for y in range(len(Pjoint[0])):
+                Pmarginal[x] += Pjoint[x][y]
+        else:
+            for y in range(len(Pjoint)):
+                Pmarginal[x] += Pjoint[y][x]
+
     return Pmarginal
-    
+
+
 def conditional_distribution_of_word_counts(Pjoint, Pmarginal):
     '''
     Parameters:
@@ -50,8 +72,12 @@ def conditional_distribution_of_word_counts(Pjoint, Pmarginal):
     Outputs: 
     Pcond (numpy array) - Pcond[m,n] = P(X1=n|X0=m)
     '''
-    raise RuntimeError('You need to write this part!')
+    Pcond = np.array([[0.0 for _ in range(len(Pjoint[0]))] for _ in range(len(Pjoint))])
+    for m in range(len(Pjoint)):
+        for n in range(len(Pjoint[0])):
+            Pcond[m][n] = Pjoint[m][n] / Pmarginal[m]
     return Pcond
+
 
 def mean_from_distribution(P):
     '''
@@ -61,8 +87,11 @@ def mean_from_distribution(P):
     Outputs:
     mu (float) - the mean of X
     '''
-    raise RuntimeError('You need to write this part!')
+    mu = 0.0
+    for n in range(len(P)):
+        mu += n * P[n]
     return mu
+
 
 def variance_from_distribution(P):
     '''
@@ -72,8 +101,12 @@ def variance_from_distribution(P):
     Outputs:
     var (float) - the variance of X
     '''
-    raise RuntimeError('You need to write this part!')
+    mu_x_squared = 0.0
+    for n in range(len(P)):
+        mu_x_squared += (n**2 * P[n])
+    var = mu_x_squared - mean_from_distribution(P)**2
     return var
+
 
 def covariance_from_distribution(P):
     '''
@@ -83,8 +116,19 @@ def covariance_from_distribution(P):
     Outputs:
     covar (float) - the covariance of X0 and X1
     '''
-    raise RuntimeError('You need to write this part!')
+    Pmarginal_0 = marginal_distribution_of_word_counts(P, 0)
+    Pmarginal_1 = marginal_distribution_of_word_counts(P, 1)
+    Ex0, Ex1 = 0.0, 0.0
+    for n in range(len(Pmarginal_0)):
+        Ex0 += n * Pmarginal_0[n]
+    for n in range(len(Pmarginal_1)):
+        Ex1 += n * Pmarginal_1[n]
+    covar = 0.0
+    for m in range(len(P)):
+        for n in range(len(P[0])):
+            covar += P[m][n] * (m - Ex0) * (n - Ex1)
     return covar
+
 
 def expectation_of_a_function(P, f):
     '''
@@ -98,6 +142,8 @@ def expectation_of_a_function(P, f):
     Output:
     expected (float) - the expected value, E[f(X0,X1)]
     '''
-    raise RuntimeError('You need to write this part!')
+    expected = 0.0
+    for x0 in range(len(P)):
+        for x1 in range(len(P[0])):
+            expected += P[x0][x1] * f(x0, x1)
     return expected
-    
