7124d63884885e18e04af1620dcd203f9eaf39a7
williamcheng <csh1234_1@126.com>
2023-03-02 11:45:26 -0600

    complete mp06 EC

--- mp06_extracredit/extracredit.py
+++ mp06_extracredit/extracredit.py
@@ -8,18 +8,32 @@ DEVICE=torch.device("cpu")
 ###########################################################################################
 def trainmodel():
     # Well, you might want to create a model a little better than this...
-    model = torch.nn.Sequential(torch.nn.Flatten(),torch.nn.Linear(in_features=8*8*15, out_features=1))
+    model = torch.nn.Sequential(
+        torch.nn.Flatten(),
+        torch.nn.Linear(in_features=8*8*15, out_features=512),
+        torch.nn.ReLU(),
+        torch.nn.Linear(512, 128),
+        torch.nn.ReLU(),
+        torch.nn.Linear(128, 1)
+    )
+
+    loss_fn = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=1e-3)
 
     # ... and if you do, this initialization might not be relevant any more ...
-    model[1].weight.data = initialize_weights()
-    model[1].bias.data = torch.zeros(1)
+    #model[1].weight.data = initialize_weights()
+    #model[1].bias.data = torch.zeros(1)
 
     # ... and you might want to put some code here to train your model:
     trainset = ChessDataset(filename='extracredit_train.txt')
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=1000, shuffle=True)
     for epoch in range(2000):
         for x,y in trainloader:
-            pass # Replace this line with some code that actually does the training
+            y_pred = model(x)
+            loss = loss_fn(y_pred, y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
     # ... after which, you should save it as "model.pkl":
     torch.save(model, 'model.pkl')
